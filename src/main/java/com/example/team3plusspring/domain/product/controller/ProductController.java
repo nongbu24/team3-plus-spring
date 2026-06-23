630af77de1dc6d5eec3219d97c12d82cfaf95d58
Sole02 <ljaeseok02@gmail.com>
2026-06-23 21:37:30 +0900

    Refactor: 코드 리뷰 반영

--- src/main/java/com/example/team3plusspring/domain/product/controller/ProductController.java
+++ src/main/java/com/example/team3plusspring/domain/product/controller/ProductController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/products")
 public class ProductController {
 
     private final ProductService productService;
 
     // 상품 상세 조회
-    @GetMapping("/products/{productId}")
+    @GetMapping("/{productId}")
     public ApiResponse<GetOneProductResponse> getProduct(@PathVariable Long productId) {
         return ApiResponse.success(productService.getProduct(productId));
     }
--- src/main/java/com/example/team3plusspring/domain/product/entity/ProductStatus.java
+++ src/main/java/com/example/team3plusspring/domain/product/entity/ProductStatus.java
@@ -3,5 +3,5 @@ package com.example.team3plusspring.domain.product.entity;
 public enum ProductStatus {
     ON_SALE,       // 판매중
     SOLD_OUT,      // 품절
-    DISCONTINUED   // 판매중단
+    DISCONTINUED   // 단종
 }
