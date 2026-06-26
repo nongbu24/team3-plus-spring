ad743276b0022d809f7496cf20a44cf756d34b22
정예진 <yxejxnn@gmail.com>
2026-06-26 19:40:23 +0900

    fix: 쿠폰 컨트롤러에 @Validated 누락 추가

--- src/main/java/com/example/team3plusspring/domain/coupon/controller/CouponEventController.java
+++ src/main/java/com/example/team3plusspring/domain/coupon/controller/CouponEventController.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/coupon-events")
+@Validated
 public class CouponEventController {
 
 	private final CouponEventService couponEventService;
--- src/main/java/com/example/team3plusspring/domain/coupon/controller/UserCouponController.java
+++ src/main/java/com/example/team3plusspring/domain/coupon/controller/UserCouponController.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users/me/coupons")
+@Validated
 public class UserCouponController {
 
 	private final UserCouponService userCouponService;
