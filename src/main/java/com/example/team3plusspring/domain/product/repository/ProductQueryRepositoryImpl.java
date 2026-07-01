7f51ccef29fe4b3c1297dee4d41bdcb6809242e8
Sole02 <ljaeseok02@gmail.com>
2026-07-01 17:50:47 +0900

    Refactor: @Repository 어노테이션 제거

--- src/main/java/com/example/team3plusspring/domain/product/repository/ProductQueryRepositoryImpl.java
+++ src/main/java/com/example/team3plusspring/domain/product/repository/ProductQueryRepositoryImpl.java
@@ -11,11 +11,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 @RequiredArgsConstructor
 public class ProductQueryRepositoryImpl implements ProductQueryRepository {
 
